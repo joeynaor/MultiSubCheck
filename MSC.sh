f6f1127162cff29fc491bfc2cb9c74b3a47ffad0
Joey <50103918+n0khodsia@users.noreply.github.com>
2019-11-18 06:24:01 -0500

    Update MSC.sh

--- MSC.sh
+++ MSC.sh
@@ -44,7 +44,7 @@ function one {
   echo -e "\e[92m[+]\e[0m Creating scans/$1 directory ..."
 	mkdir scans/$1
 	echo -e "Sanning subdomains on $1..."
-	sublist3r -d $1 -o scans/$1/subdomains.list || echo "Could not locate sublist3r. Please install it and move it to /usr/bin"
+	python sublist3r -d $1 -o scans/$1/subdomains.list || echo "Could not launch sublist3r. Please follow the installation process on github."
 	echo -e "\e[92m[+]\e[0m Scan done. list saved to scans/$1/subdomains.list"
 }
 
